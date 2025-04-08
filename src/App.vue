d680a725dfd260f76a3bed420659a0b5084e3ac4
AzureNights <kokilavanielamparidhi@gmail.com>
2025-04-08 16:35:19 +0800

    Created the form that works, and on submit, the data is saved on the console

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import AddContact from './components/AddContact.vue';
 
 </script>
@@ -8,11 +6,7 @@ import AddContact from './components/AddContact.vue';
 <template>
   <header>
     
-    <div>
-      <h1>Contacts App</h1>
-    </div>
-
-
+    
   </header>
 
   <main>
